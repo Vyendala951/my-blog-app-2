b4bd850baab2af4a7e31d7df09dc7ef0171383d2
vinay <vyendala@myune.edu.au>
2025-08-17 02:13:18 +0545

    created post and category models and edited user model

---
+++ app/Models/Post.php
@@ -0,0 +1,20 @@
+<?php
+namespace App\Models;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model
+{
+    use HasFactory;
+    protected $fillable = ['title', 'content', 'user_id', 'category_id', 'is_active'];
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+}
