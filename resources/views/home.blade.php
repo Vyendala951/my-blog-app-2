e68ae422fe7fd1fb7dee88531c514e92cc9e2f95
vinay <vyendala@myune.edu.au>
2025-08-17 12:45:25 +0545

    user access limited in homepage

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,7 +5,7 @@
 @section('content')
     <div class="d-flex justify-content-between align-items-center mb-3">
         <h2 class="mb-0">Posts</h2>
-        <a href="{{ route('posts.create') }}" class="btn btn-primary">+ Create New Post</a>
+        <a href="#" class="btn btn-primary">+ Create New Post</a>
     </div>
 
     <table class="table table-bordered table-striped">
@@ -16,7 +16,6 @@
                 <th>Content</th>
                 <th>Category</th>
                 <th>Created At</th>
-                <th style="width: 150px;">Actions</th>
             </tr>
         </thead>
         <tbody>
@@ -27,14 +26,6 @@
                     <td>Lorem ipsum dolor sit amet, consectetur adipiscing elit. {{ $i }}</td>
                     <td>Lorem {{ $i }}</td>
                     <td>{{ now()->subDays(10 - $i)->format('d M Y') }}</td>
-                    <td>
-                        <a href="#" class="btn btn-sm btn-warning">Edit</a>
-                        <form action="#" method="POST" class="d-inline" onsubmit="return confirm('Delete this post?');">
-                            @csrf
-                            @method('DELETE')
-                            <button class="btn btn-sm btn-danger">Delete</button>
-                        </form>
-                    </td>
                 </tr>
             @endfor
         </tbody>
